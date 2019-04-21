ac247bdbc81aebbb073d6b962973e904f3412af4
Anuphon Chaisiri <49830081+anuphonchaisiri@users.noreply.github.com>
2019-04-21 17:00:22 +0700

    Add files via upload

---
+++ ROMWeb/Service/AuthUser.cs
@@ -0,0 +1,70 @@
+﻿using COMLibrary;
+using COMLibrary.Model.Company;
+using COMLibrary.Model.User;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ROM.Service
+{
+    public class AuthUser
+    {
+        
+        public AuthUser() { }
+        public static bool Login(string username,string password)
+        {
+            //remove cookie
+            HttpContext.Current.Response.Cookies.Remove("COOKIE_ROMWEB_USER_INFO");
+            _data = LoginService.getInstance().Login("COM",username,password);
+            if (_data == null || string.IsNullOrEmpty(_data.Username))
+            {
+                return false;
+            }
+            //add cookie
+            HttpCookie cookName = new HttpCookie("COOKIE_ROMWEB_USER_INFO");
+            cookName.Value = JsonConvert.SerializeObject(_data);
+            HttpContext.Current.Response.Cookies.Add(cookName);
+            return true;
+        }
+
+        public static bool SetAuthPublic(CompanyInfoModel company) {
+            HttpContext.Current.Response.Cookies.Remove("COOKIE_ROMWEB_USER_INFO");
+            HttpCookie cookName = new HttpCookie("COOKIE_ROMWEB_USER_INFO");
+            _data = new UserDataEntity();
+            _data.SID = company.sid;
+            cookName.Value = JsonConvert.SerializeObject(_data);
+            HttpContext.Current.Response.Cookies.Add(cookName);
+            return true;
+        }
+
+        private static UserDataEntity _data { get; set; }
+        private static UserDataEntity data
+        {
+            get
+            {
+                if (_data == null || string.IsNullOrEmpty(_data.SID))
+                {
+                    if (HttpContext.Current.Request.Cookies["Cookie_AUTH_URL"] != null)
+                    {
+                        _data = JsonConvert.DeserializeObject<UserDataEntity>(HttpContext.Current.Request.Cookies["COOKIE_ROMWEB_USER_INFO"].Value);
+                    }
+                    else
+                    {
+                        _data = new UserDataEntity();
+                    }
+                }
+                return _data;
+            }
+        }
+
+        public static string SID { get { return data.SID; } }
+        public static string UserId { get { return data.UserId; } }
+        public static string Username { get { return data.Username; } }
+        public static string FullName { get { return data.FullName; } }
+        public static string EmployeeCode { get { return data.EmployeeCode; } }
+
+        
+    }
+}
---
+++ ROMWeb/Service/DBConnect.cs
@@ -0,0 +1,27 @@
+﻿using COMLibrary;
+using COMLibrary.Model.Finance;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+
+namespace ROM.Service
+{
+    public class DBConnect : DbContext
+    {
+       // public String DBCenterConnectionString { get { return Convert.ToString("Provider=SQLOLEDB.1;" + System.Configuration.ConfigurationManager.ConnectionStrings["DBCenterConnectionStringEntity"]); } }
+       // public String DBLocalConnectionString { get { return Convert.ToString(System.Web.HttpContext.Current.Session["DBLocalConnectionString"]); } }
+       
+        public DBConnect() : base("DBCenterConnectionStringEntity")
+        {
+            Database.SetInitializer<DBConnect>(new CreateDatabaseIfNotExists<DBConnect>());
+        }
+        
+        public DbSet<AccountMsaterModel> master_account { get; set; }
+        
+    }
+}
