0ed3376ae3068fcf49e2839179142cdf7a2b7874
Anuphon Chaisiri <49830081+anuphonchaisiri@users.noreply.github.com>
2019-05-06 17:33:33 +0700

    เพิ่มหน้าจอ โครงสร้างองค์กร

---
+++ ROMWeb/Controllers/CompanyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ROM.Controllers
+{
+    public class CompanyController : TemplateController
+    {
+        // GET: Company
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
